9420012ee84770d14a0b8e2c76615ae16d8a746a
Nicholas Cessna <157065013+NicholasCessna@users.noreply.github.com>
2024-11-29 21:18:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 import requests
 import os
+import geocoder
 from plyer import gps
 
 
@@ -10,23 +12,18 @@ class GPSApp(App):
         self.label = Label(text="Waiting for GPS data...")
         layout = BoxLayout(orientation='vertical')
         layout.add_widget(self.label)
-        
-        if platform == 'android':
-            self.request_android_permissions()
-            gps.start()
-            gps.configure(on_location=self.update_location)
-        else:
-            self.label.text = "GPS is not supported on this platform"
+        g = geocoder.ip("me")
+        self.update_location(coords=g.latlng)
+
 
         return layout
 
-    def request_android_permissions(self):
-        request_permissions([Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION])
-        return self.label
 
-    def update_location(self, lat, lon):
-        response = requests.get(f"https://api.geoapify.com/v2/places?categories=commercial.supermarket,commercial.food_and_drink&filter=circle:{lat},{lon},5000&limit=20&apiKey=95d1097e14424eb28f6d468a8e8745ad")
+    def update_location(self, coords):
+        print(coords[0],coords[1])
+        response = requests.get(f"https://api.geoapify.com/v2/places?categories=commercial&filter=circle:{coords[0]},{coords[1]},15000&limit=20&apiKey=95d1097e14424eb28f6d468a8e8745ad")
         webdata = response.json()
+        print(webdata)
         
         # Extracting names of places from the web data
         place_names = [feature["properties"]["name"] for feature in webdata.get("features", [])]
